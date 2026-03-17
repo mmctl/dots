0fc621e6d04d4efa2dbe3b04c0aad45f5826562a
mmctl <kernel@mmeijers.com>
2026-03-17 08:16:47 +0100

    Initial starting point.

--- zsh/dot-config/zsh/aliases.zsh
+++
@@ -1,9 +0,0 @@
-# LS
-alias ls='ls --color=tty'
-
-# Yazi
-alias y='yazi'
-
-# Emacs
-alias ect='emacsclient -t --alternate-editor='
-alias ecc='emacsclient -c -n --alternate-editor='
--- zsh/dot-config/zsh/prompt.zsh
+++
@@ -1,5 +0,0 @@
-export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/starship.toml"
-
-eval "$(starship init zsh)"
-
-export STARSHIP_CACHE="$XDG_CACHE_HOME/starship/session-${STARSHIP_SESSION_KEY}.log"
