a8786a1e5d4414c634056e293879a60114cf3ad1
mmctl <kernel@mmeijers.com>
2026-06-28 13:27:32 +0200

    Explicitly queue all installed packages for native compilation

--- emacs/dot-local/share/emacs/install/wait-native-comp.el
+++ emacs/dot-local/share/emacs/install/wait-native-comp.el
@@ -3,6 +3,12 @@
 ;; Meant to be run at the end of initial non-interactive installation/setup
 ;; (e.g., via `--batch')
 
+;; Recursively queue all installed packages for native compilation
+(when (and (fboundp 'native-comp-available-p)
+           (native-comp-available-p))
+  (native-compile-async (list package-user-dir) t))
+
+;; Wait for native compilation processes to finish
 (message "Waiting for native compilation to finish...")
 
 (when (and (fboundp 'native-comp-available-p)
