812878af4bf6f7edd63c4c0cee51d0b749389168
mmctl <kernel@mmeijers.com>
2026-07-03 11:57:08 +0200

    Add FZF options for TAB

--- zsh/dot-config/zsh/extensions/99-plugins.zsh
+++ zsh/dot-config/zsh/extensions/99-plugins.zsh
@@ -6,6 +6,18 @@
     if (( $+commands[fzf] )) &&
            [[ -r "$plugdir/fzf-tab/fzf-tab.plugin.zsh" ]]; then
         source "$plugdir/fzf-tab/fzf-tab.plugin.zsh"
+        zstyle ':fzf-tab:*' fzf-flags \
+               --height='~60%' \
+               --wrap \
+               --scroll-off=3 \
+               --hscroll-off=10 \
+               --layout=reverse \
+               --border=rounded \
+               --margin=0,2.5% \
+               --padding=0 \
+               --info=inline-right \
+               --ansi \
+               --highlight-line
     fi
 
     [[ -r "$plugdir/zsh-autosuggestions/zsh-autosuggestions.zsh" ]] &&
