7fc6fc81007e3bf09c4cd138a862dc34ef0707b6
mmctl <kernel@mmeijers.com>
2026-07-14 17:09:59 +0200

    local nerd font set, not sure

--- emacs/dot-local/share/emacs/local/local-nerd-icons.el
+++ emacs/dot-local/share/emacs/local/local-nerd-icons.el
@@ -7,10 +7,10 @@
 (defun an-after-advice-local-setup-gui-nerd-icons-font (frame &rest _)
   "Configure the Nerd Icons font mapping for FRAME.
 
-Meant as after advice for `local-setup-gui-frame', so FRAME
-is assumed to be a GUI frame."
-   (message "frame: %s" frame)
-  (nerd-icons-set-font nil frame))
+Meant as after advice for `local-setup-gui-frame' (and also only makes
+sense for GUI frames)."
+  (when (display-graphic-p frame)
+    (nerd-icons-set-font nil frame)))
 
 
 (provide 'local-nerd-icons)
